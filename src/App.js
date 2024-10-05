1db5714905e8790534b97d3409c0c9266f200cbb
Ganesh907 <ganishinde907@gmail.com>
2024-10-05 16:37:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import CelebrationIcon from '@mui/icons-material/Celebration';
+function App() {
+  return (
+
+
+    <div className='h-screen flex justify-center items-center'>
+      <CelebrationIcon sx={{ fontSize: "100px" }} />
+      <h1 className='text-4xl italic mx-10 border-y-4 py-2 border-black'>
+        Classic Event
+      </h1>
+      <CelebrationIcon sx={{ fontSize: "100px", transform: "rotate(270deg)" }} />
+    </div>
+
+  );
+}
+
+export default App;
