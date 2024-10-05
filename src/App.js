914e79a3445dce878142ab014248f404b10fb8a9
bitbybitgithub <bitbybitsolutionsofficial.com>
2024-10-05 20:08:17 +0530

    RD(ContactUsForm)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import CelebrationIcon from '@mui/icons-material/Celebration';
+import CelebrationIcon from "@mui/icons-material/Celebration";
+import { ContactUsForm } from "../src/components/contactuspage/ContactUsForm";
 function App() {
   return (
+    <div className="h-screen flex justify-center items-center">
+      {/* <CelebrationIcon sx={{ fontSize: "100px" }} />
 
-
-    <div className='h-screen flex justify-center items-center'>
-      <CelebrationIcon sx={{ fontSize: "100px" }} />
-      <h1 className='text-4xl italic mx-10 border-y-4 py-2 border-black'>
+      <h1 className="text-4xl italic mx-10 border-y-4 py-2 border-black">
         Classic Event
       </h1>
-      <CelebrationIcon sx={{ fontSize: "100px", transform: "rotate(270deg)" }} />
+      <CelebrationIcon
+        sx={{ fontSize: "100px", transform: "rotate(270deg)" }}
+      /> */}
+      <ContactUsForm />
     </div>
-
   );
 }
 
--- src/components/contactuspage/ContactUsForm.js
+++ src/components/contactuspage/ContactUsForm.js
@@ -1,7 +1,129 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const ContactUsForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Form data submitted: ", formData);
+  };
+
   return (
-    <div>ContactUsForm</div>
-  )
-}
+    // <div>ContactUsForm</div>
+    <div className="bg-white rounded-lg shadow-lg w-96">
+      <div className=" bg-gray-600 h-12 flex items-center justify-center">
+        <h2 className="text-2xl text-white">Send Us Enquiry</h2>
+      </div>
+
+      <div className="mx-4">
+        <label
+          htmlFor="name"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+          className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+        />
+      </div>
+
+      <div className="mx-4">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+          className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+        />
+      </div>
+
+      <div className="mx-4">
+        <label
+          htmlFor="phone"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Phone
+        </label>
+        <input
+          type="tel"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+          className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+        />
+      </div>
+
+      <div className="mx-4">
+        <label
+          htmlFor="subject"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Subject
+        </label>
+        <input
+          type="text"
+          name="subject"
+          value={formData.subject}
+          onChange={handleChange}
+          required
+          className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+        />
+      </div>
+
+      <div className="mx-4">
+        <label
+          htmlFor="message"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Message
+        </label>
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+          className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+          rows="4"
+        ></textarea>
+      </div>
+
+      <div className="mx-4 my-4">
+        <button
+          type="submit"
+          onChange={handleSubmit}
+          className="w-full bg-[#ff0000] border border-transparent text-white p-2 rounded-md hover:bg-white hover:border-[#ff0000] hover:text-[#ff0000] transition duration-200"
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
